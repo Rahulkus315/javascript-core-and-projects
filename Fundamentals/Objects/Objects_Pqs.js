deb24f2c6af0423ce4df72ac757059e7c84adb1a
Rahul kushwaha <rahulkushwha181@gmail.com>
2026-02-27 20:04:45 +0530

    Added some Ojects practice Questions

---
+++ Fundamentals/Objects/Objects_Pqs.js
@@ -0,0 +1,22 @@
+// ============================ Objects_Pqs.js ============================
+// Here are some practice questions related to objects in JavaScript:
+// 1. Create an object called "car" with properties "make", "model", and "year".
+// 2. Access the "model" property of the "car" object and print it to the console.
+// 3. Add a new property called "color" to the "car" object and set its value to "red".
+// 4. Delete the "year" property from the "car" object.
+// 5. Check if the "make" property exists in the "car" object and print the result to the console.
+// 6. Iterate over the properties of the "car" object and print each key and its corresponding value to the console.
+// 7. Get the keys of the "car" object and print them to the console.
+// 8. Get the values of the "car" object and print them to the console.
+// 9. Get the entries of the "car" object and print them to the console.    
+// 10. Create a new object called "person" with properties "name", "age", and "city". Then, create a function that takes an object as an argument and prints the values of its properties to the console. Call this function with the "person" object as an argument.
+
+// ==================== Some Advance Object Practice Questions ====================
+// 1. Create a constructor function called "Person" that takes in parameters "name", "age", and "city". Use this constructor function to create a new object called "person1" with the values "Alice", 25, and "Los Angeles". Then, print the properties of the "person1" object to the console.
+// 2. Create a class called "Animal" with a constructor that takes in parameters "name" and "species". Add a method called "speak" that prints a message to the console. Then, create a new object called "animal1" using the "Animal" class with the values "Dog" and "Canine". Call the "speak" method on the "animal1" object.
+// 3. Create an object called "book" with properties "title", "author", and "year". Then, create a function that takes in an object as an argument and returns a string in the format "Title: [title], Author: [author], Year: [year]". Call this function with the "book" object as an argument and print the result to the console.
+// 4. Create an object called "student" with properties "name", "grade", and "subjects". The "subjects" property should be an array of strings representing the subjects the student is taking. Then, create a function that takes in an object as an argument and prints the student's name and the subjects they are taking to the console. Call this function with the "student" object as an argument.
+// 5. Create a class called "Rectangle" with a constructor that takes in parameters "width" and "height". Add a method called "area" that calculates and returns the area of the rectangle. Then, create a new object called "rectangle1" using the "Rectangle" class with the values 5 and 10. Call the "area" method on the "rectangle1" object and print the result to the console.
+
+
+// ==================== End of Objects_Pqs.js ========================
